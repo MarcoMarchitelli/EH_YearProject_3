573e314bcc0e1ade7336fcc343d3a2278bcb9470
MarcoMarchitelli <igvvcdhk@gmail.com>
2020-01-13 18:56:58 +0100

    Added sounds. Tweeners fix. Added cinemachine. Added screen shake on placement. Placement particles. Added asset references finder plugin.

---
+++ Assets/Deirin/Utilities/Chinemachine Monos/CinemachineNoiseController.cs
@@ -0,0 +1,60 @@
+﻿namespace Deirin.Utilities {
+    using UnityEngine;
+    using Cinemachine;
+    using UnityEngine.Events;
+    using DG.Tweening;
+
+    public class CinemachineNoiseController : MonoBehaviour {
+        [Header("Refs")]
+        public CinemachineVirtualCamera cam;
+
+        [Header("Params")]
+        public bool setupOnAwake = true;
+        [Space]
+        //public NoiseSettings noiseProfile;
+        public float amplitudeGain;
+        public float frequencyGain;
+        public Vector3 pivotOffset;
+        [Space]
+        public float duration;
+
+        [Header("Events")]
+        public UnityEvent OnPlay;
+        public UnityEvent OnEnd;
+
+        private CinemachineBasicMultiChannelPerlin noise;
+        private Sequence s;
+
+        private void Awake () {
+            if ( setupOnAwake )
+                Setup();
+        }
+
+        public void Setup () {
+            noise = cam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        }
+
+        public void Play () {
+            noise.m_AmplitudeGain = amplitudeGain;
+            noise.m_FrequencyGain = frequencyGain;
+            noise.m_PivotOffset = pivotOffset;
+            //noise.m_NoiseProfile = noiseProfile;
+            s = DOTween.Sequence();
+            s.AppendInterval( duration );
+            s.onComplete += End;
+            OnPlay.Invoke();
+        }
+
+        public void Stop () {
+            s.Kill();
+            End();
+        }
+
+        private void End () {
+            noise.m_AmplitudeGain = 0;
+            noise.m_FrequencyGain = 0;
+            noise.m_PivotOffset = Vector3.zero;
+            OnEnd.Invoke();
+        }
+    }
+}
