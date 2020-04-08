50dce04d630f2c7a7704efeef975f964a9688818
Marco Marchitelli <marchitelli97@libero.it>
2020-04-08 17:08:53 +0200

    Mortar shooter first implementation.

--- Assets/0_Deirin/Entity-Behaviour System/Behaviours/Interpolator.cs
+++ Assets/0_Deirin/Entity-Behaviour System/Behaviours/Interpolator.cs
@@ -1,13 +1,17 @@
 ﻿namespace Deirin.EB {
     using System.Collections;
-    using System.Collections.Generic;
     using UnityEngine;
+    using UnityEngine.Events;
 
     public class Interpolator : BaseBehaviour {
         [Header("Parameters")]
         public AnimationCurve curve;
         public float speed;
+        public float curveValueMultiplier = 1;
         public bool setStartPosOnSetup = true;
+
+        [Header("Events")]
+        public UnityEvent OnTargetReached;
 
         private Vector3 targetPosition;
         private Vector3 startPosition;
@@ -32,8 +36,14 @@
         IEnumerator Interpolation () {
             float percent = 0;
             while ( percent < 1 ) {
+                percent += speed * Time.deltaTime;
+                Vector3 linearPos = Vector3.Lerp(startPosition, targetPosition, percent);
+                float y = curve.Evaluate( percent ) * curveValueMultiplier;
+                linearPos.Set( linearPos.x, linearPos.y + y, linearPos.z );
+                transform.position = linearPos;
                 yield return null;
             }
+            OnTargetReached.Invoke();
         }
     }
 }
---
+++ Assets/0_Deirin/Entity-Behaviour System/Behaviours/OverlapDamageDealer.cs
@@ -0,0 +1,49 @@
+﻿namespace Deirin.EB {
+    using UnityEngine;
+
+    public class OverlapDamageDealer : BaseBehaviour {
+        public enum GetComponentMode { self, parent, children }
+
+        [Header("Parameters")]
+        public float damage;
+        public float radius;
+        public GetComponentMode getComponentMode;
+
+        private Collider[] hitObjs;
+        private DamageReceiver damageReceiver;
+
+        public void UpdateOverlap () {
+            hitObjs = Physics.OverlapSphere( transform.position, radius );
+
+            CheckObjects();
+        }
+
+        private void CheckObjects () {
+            switch ( getComponentMode ) {
+                case GetComponentMode.self:
+                for ( int i = 0; i < hitObjs.Length; i++ ) {
+                    if ( hitObjs[i].TryGetComponent( out damageReceiver ) ) {
+                        damageReceiver.Damage( damage );
+                    }
+                }
+                break;
+                case GetComponentMode.parent:
+                for ( int i = 0; i < hitObjs.Length; i++ ) {
+                    damageReceiver = hitObjs[i].GetComponentInParent<DamageReceiver>();
+                    if ( damageReceiver ) {
+                        damageReceiver.Damage( damage );
+                    }
+                }
+                break;
+                case GetComponentMode.children:
+                for ( int i = 0; i < hitObjs.Length; i++ ) {
+                    damageReceiver = hitObjs[i].GetComponentInChildren<DamageReceiver>();
+                    if ( damageReceiver ) {
+                        damageReceiver.Damage( damage );
+                    }
+                }
+                break;
+            }
+        }
+    }
+}
