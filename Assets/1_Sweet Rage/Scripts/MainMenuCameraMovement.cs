414624bff22d7f17e7aab83fc9af37bb5e15c3b8
Marco Marchitelli <marchitelli97@libero.it>
2020-06-18 20:05:36 +0200

    Main menu camera movement and blur.

---
+++ Assets/1_Sweet Rage/Scripts/MainMenuCameraMovement.cs
@@ -0,0 +1,20 @@
+﻿namespace SweetRage {
+    using UnityEngine;
+
+    public class MainMenuCameraMovement : MonoBehaviour {
+        public float intesity = 10;
+        [Range(0,1)] public float speed = 1;
+
+        private Vector3 startPosition;
+
+        private void Awake () {
+            startPosition = transform.localPosition;
+        }
+
+        private void Update () {
+            float amount = Time.time * speed;
+            Vector3 movement = new Vector3( Mathf.Cos(amount) * intesity, Mathf.Sin(amount) * intesity, 0 );
+            transform.localPosition = startPosition + movement;
+        }
+    }
+}
