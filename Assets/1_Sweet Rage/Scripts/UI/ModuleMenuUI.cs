22088bf9927cc9b54c5c9bf9ffaae2636c626d53
MarcoMarchitelli <igvvcdhk@gmail.com>
2020-07-03 18:01:28 +0200

    Modules Menu animations are now based on unscaled time. Gameplay UI is now NOT clickable until level enter animation end.

--- Assets/1_Sweet Rage/Scripts/UI/ModuleMenuUI.cs
+++ Assets/1_Sweet Rage/Scripts/UI/ModuleMenuUI.cs
@@ -23,7 +23,7 @@
             //adjust width
             float targetWidth = count * elementWidth + horizontalSpaceing * 2 + (count - 1) * elementsDistance;
             Vector2 targetSize = new Vector2( targetWidth, count > 0 ? startingHeight : startingHeight * .5f );
-            self.sizeDelta = Vector2.MoveTowards( self.sizeDelta, targetSize, Time.deltaTime * speed );
+            self.sizeDelta = Vector2.MoveTowards( self.sizeDelta, targetSize, Time.unscaledDeltaTime * speed );
 
             //adjust elements pos
             int i = 1;
@@ -32,7 +32,7 @@
 
                 float xPos = horizontalSpaceing + (i - 1) * elementWidth + (i - 1) * elementsDistance + elementWidth * .5f;
                 Vector2 targetPos = new Vector2( xPos, self.sizeDelta.y * .5f );
-                element.anchoredPosition = Vector2.MoveTowards( element.anchoredPosition, targetPos, Time.deltaTime * speed );
+                element.anchoredPosition = Vector2.MoveTowards( element.anchoredPosition, targetPos, Time.unscaledDeltaTime * speed );
                 i++;
             }
         }
