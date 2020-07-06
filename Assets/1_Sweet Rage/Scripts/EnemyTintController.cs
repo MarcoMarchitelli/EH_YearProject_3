e6b346fe1df57e7ad5806481720a28ccd6164ca3
Marco Marchitelli <marchitelli97@libero.it>
2020-07-06 22:08:36 +0200

    Enemy burning and freezing effects.

---
+++ Assets/1_Sweet Rage/Scripts/EnemyTintController.cs
@@ -0,0 +1,20 @@
+﻿namespace SweetRage {
+    using UnityEngine;
+
+    public class EnemyTintController : MonoBehaviour {
+        public SkinnedMeshRenderer smr;
+        public Color color;
+
+        private int tintID;
+
+        private void Awake () {
+            tintID = Shader.PropertyToID( "_Tint" );
+        }
+
+        public void SetColor () {
+            foreach ( var mat in smr.materials ) {
+                mat.SetColor( tintID, color );
+            }
+        }
+    } 
+}
