8d9533e6b7deb30f27047afd7ce7ee98f723f850
Marco Marchitelli <marchitelli97@libero.it>
2020-07-25 21:50:17 +0200

    Module interaction sounds and enemy death sounds.

---
+++ Assets/0_Deirin/Utilities/Generic Monos/AudioSourceUtilities.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class AudioSourceUtilities : MonoBehaviour {
+    public AudioSource audioSource;
+
+    public UnityEvent OnPlayEnd;
+    public UnityEvent OnPlay;
+
+    private float duration;
+
+    public void Awake () {
+        duration = audioSource.clip.length;
+    }
+
+    public void Play () {
+        audioSource.Play();
+    }
+}
---
+++ Assets/0_Deirin/Utilities/Generic Monos/RandomPitch.cs
@@ -0,0 +1,18 @@
+﻿namespace Deirin.Utilities {
+    using UnityEngine;
+
+    public class RandomPitch : MonoBehaviour {
+        public AudioSource audioSource;
+
+        public float min, max;
+
+        public void RandomizePitch () {
+            audioSource.pitch = Random.Range( min, max );
+        }
+
+        public void PlayRandomized () {
+            RandomizePitch();
+            audioSource.Play();
+        }
+    }
+}
